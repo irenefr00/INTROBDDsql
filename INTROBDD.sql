9cf39eadadd62852c7db39d0aae68b38589783ec
irenefr00 <irenefr00@hotmail.com>
2025-07-22 13:14:32 +0200

    Create INTROBDD.sql

---
+++ INTROBDD.sql
@@ -0,0 +1,5 @@
+SELECT flight_id, flight_no, status, FROM flights WHERE status = 'On Time’;
+SELECT * FROM bookings WHERE total_amount >= 1000000;
+SELECT * FROM aircraft_data;
+SELECT flight_id, flight_no FROM flights WHERE aircraft_code = '737';
+SELECT * FROM tickets WHERE passenger_name LIKE 'IRINA%';
